10528f166276887b99ee135d2cad078203ec97c6
Isaac Aku <isaac.aku@svr.cyphercrescent.com>
2023-12-05 14:40:37 +0100

    books-launch

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="min-h-screen bg-red-300 bg2 py-36 px-6 3xl:px-96 md:px-48 md:py-48 ">
+    <div class="h-full flex-1 flex-col">
+      <h1 class="heading">
+        <span bg-red-500>Events</span>
+        <div class="container mx-auto px-4">
+        <h2 class="bg-red-500 text-2xl md:text-4xl font-semibold text-center mb-4">
+          Our Solutions
+        </h2>
+        <p class="text-2xl text-center">
+          We are known as high-skilled marketplace and we see an acute pain
+          point within that area,
+        </p></div>
+      </h1>
+    </div>
+  </div>
+  </div>
+
+</template>
